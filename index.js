389a03c8e567cbb6557fa338deafecd27a5846af
tarikulsk <tarikulsk4@gmai.com>
2023-05-03 10:04:18 +0530

    api call

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
 const recipes = require('./data/recipes.json')
+const recipe = require('./data/recipe.json')
+
+const cors = require('cors')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.get('/racipe', (req, res) => {
+app.use(cors())
+
+app.get('/recipes', (req, res) => {
     res.send(recipes)
+})
+app.get('/recipe', (req, res) => {
+    res.send(recipe)
+})
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const viewrecipe = recipe.find(n => n.id === id)
+    res.send(viewrecipe);
+    // res.send(recipes)
 })
 
 app.listen(port, () => {
